9c2d43a297e51d8b3bfca7fb06b1c1dd5be7d894
Jasa Paavolainen <jasa.paavolainen@tuni.fi>
2020-10-02 13:11:38 +0300

    Added movement, collectables and particles

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    public GameObject particleFX;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("It worked!");
+        Instantiate(particleFX, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+
+    }
+
+}
